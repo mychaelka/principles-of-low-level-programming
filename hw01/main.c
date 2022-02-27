0d14e281a42384e969d711a3f6f8d022136cdead
mychaela98 <mychaela.k98@gmail.com>
2022-02-27 11:38:29 +0100

    added base switches

--- hw01/main.c
+++ hw01/main.c
@@ -10,41 +10,168 @@ void print_error_message(char *message)
 }
 
 
-unsigned long long int read_command(unsigned long long int acc, int last_op) // acc = 0 at first
+void print_base(uint64_t num, int base)
+{
+    uint64_t rem;
+
+    if (num == 0){
+        printf("# 0\n");
+        return;
+    }
+
+    char result[10000];
+    int i, idx = 0;
+    while (num > 0){
+        rem = num % base;
+        if (rem < 10){
+            rem += 48;
+        }
+        else {
+            rem += 55;
+        }
+        result[idx] = rem;
+
+        num /= base;
+        idx++;
+    }
+    printf("# ");
+    for (i = idx-1; i >= 0; i--){
+        printf("%c", result[i]);
+    }
+    printf("\n");
+}
+
+
+uint64_t read_command(uint64_t acc, int last_op) // acc = 0 at first
 {
     int current = getchar();
     bool non_digit = true;
+    bool hex_switcher =  false;
+    bool okt_switcher = false;
+    bool bin_switcher = false;
     while (true){//current != ';' && current != '='){
-        unsigned long long int out = 0;
-
-        //printf("last nonempty: %c\n", last_nonempty);
-        //printf("current: %c\n", current);
+        uint64_t out = 0;
 
         if (isspace(current)){  // ignore whitespaces
             current = getchar();
             continue;
         }
 
-        if (current == EOF){
-            printf("# %llu\n", acc);
-            exit(0);
+        if (current == 'X'){ // now this will expect a number
+            if (hex_switcher || okt_switcher || bin_switcher){ // switcher already in place
+                print_error_message("SYNTAX ERROR");
+                exit(1);
+            }
+            hex_switcher = true;
+            print_base(acc, 16);
+            current = getchar();
+            continue;
+        }
+        else if (current == 'O'){
+            if (hex_switcher || okt_switcher || bin_switcher){ // switcher already in place
+                print_error_message("SYNTAX ERROR");
+                exit(1);
+            }
+            okt_switcher = true;
+            print_base(acc, 8);
+            current = getchar();
+            continue;
+        }
+        else if (current == 'T'){
+            if (hex_switcher || okt_switcher || bin_switcher){ // switcher already in place
+                print_error_message("SYNTAX ERROR");
+                exit(1);
+            }
+            bin_switcher = true;
+            print_base(acc, 2);
+            current = getchar();
+            continue;
         }
 
-        // reading numbers with whitespaces
-        while ((current >= '0' && current <= '9') || isspace(current)) {
-            int num = current - '0';
+        if (hex_switcher){ // base switchers
+            while (isxdigit(current) || isspace(current)){
+                int num = current - '0';
 
-            if (!isspace(current)){
-                non_digit = false;
-                out *= 10;
-                out += num;
+                if (!isspace(current)){
+                    hex_switcher = false;
+                    non_digit = false;
+                    if (current == 'A' || current == 'a'){
+                        out *= 16;
+                        out += 10;
+                    }
+                    else if (current == 'B' || current == 'b'){
+                        out *= 16;
+                        out += 11;
+                    }
+                    else if (current == 'C' || current == 'c'){
+                        out *= 16;
+                        out += 12;
+                    }
+                    else if (current == 'D' || current == 'd'){
+                        out *= 16;
+                        out += 12;
+                    }
+                    else if (current == 'E' || current == 'e'){
+                        out *= 16;
+                        out += 12;
+                    }
+                    else if (current == 'F' || current == 'f'){
+                        out *= 16;
+                        out += 12;
+                    }
+                    else {
+                        out *= 16;
+                        out += num;
+                    }
+                }
+                current = getchar();
             }
-            current = getchar();
+        }
+        else if (okt_switcher){
+            while ((current >= '0' && current <= '7') || isspace(current)){
+                int num = current - '0';
+
+                if (!isspace(current)){
+                    okt_switcher = false;
+                    non_digit = false;
+                    out *= 8;
+                    out += num;
+                }
+                current = getchar();
+            }
+        }
+        else if (bin_switcher){
+            while (current == '0' || current == '1' || isspace(current)){
+                int num = current - '0';
+
+                if (!isspace(current)){
+                    bin_switcher = false;
+                    non_digit = false;
+                    out *= 2;
+                    out += num;
+                }
+                current = getchar();
+            }
+        }
+        else {
+            // reading numbers with whitespaces
+            while ((current >= '0' && current <= '9') || isspace(current)) {
+                int num = current - '0';
+
+                if (!isspace(current)){
+                    non_digit = false;
+                    out *= 10;
+                    out += num;
+                }
+                current = getchar();
+            }
         }
 
         if (non_digit){
-            print_error_message("SYNTAX ERROR");
-            exit(1);
+            if (current != 'X' && current != 'O' && current != 'T'){
+                print_error_message("SYNTAX ERROR");
+                exit(1);
+            }
         }
 
         if (last_op == 'P'){
@@ -77,15 +204,14 @@ unsigned long long int read_command(unsigned long long int acc, int last_op) // 
         }
 
         if (current == 'N'){
-            printf("# %llu\n", acc);
+            printf("# %lu\n", acc);
             acc = 0;
-            printf("# %llu\n", acc);
-            //current = getchar();
+            printf("# %lu\n", acc);
             return acc;
         }
 
         if (current == '='){
-            printf("# %llu\n", acc);
+            printf("# %lu\n", acc);
             break;
         }
         if (current == ';'){
@@ -94,36 +220,40 @@ unsigned long long int read_command(unsigned long long int acc, int last_op) // 
             }
             break;
         }
+        if (current == EOF){
+            printf("# %lu\n", acc);
+            exit(0);
+        }
 
         // when next character is an operator
         if (current == '+' || current == '-' || current == '*' || current == '/'
             || current == '%' || current == '<' || current == '>' || current == 'P'){
-            printf("# %llu\n", acc);
+            printf("# %lu\n", acc);
             last_op = current;
             non_digit = true;
             current = getchar();
             continue;
         }
-
-        current = getchar();
-        if (!isspace(current)){
-            non_digit = true;
+        else {
+            printf("# %lu\n", acc);
+            print_error_message("SYNTAX ERROR");
+            exit(1);
         }
     }
 
-    printf("# %llu\n", acc);
+    printf("# %lu\n", acc);
     return acc;
 }
 
 
 bool calculate(void)
 {
-    unsigned long long int acc = 0;
+    unsigned long int acc = 0;
     int ch;
     while ((ch=getchar()) != EOF){ // 1 loop = loading of 1 character from stdin
         if (ch == 'N'){
             acc = 0;
-            printf("# %llu\n", acc);
+            printf("# %lu\n", acc);
         }
 
         else if (ch == ';'){ // in case of ';' continue
@@ -134,12 +264,25 @@ bool calculate(void)
         }
 
         else if (ch == '='){ // in case of '=' print acc and continue
-            printf("# %llu\n", acc);
+            printf("# %lu\n", acc);
         }
 
         else if (ch == 'P' || ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%'
                  || ch == '<' || ch == '>'){
             acc = read_command(acc, ch);
+        }
+
+        else if (ch == 'X'){
+            print_base(acc, 16);
+            continue;
+        }
+        else if (ch == 'O'){
+            print_base(acc, 8);
+            continue;
+        }
+        else if (ch == 'T'){
+            print_base(acc, 2);
+            continue;
         }
 
         else if (isspace(ch)){
